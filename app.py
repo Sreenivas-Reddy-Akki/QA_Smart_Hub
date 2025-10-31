5cced6cd2918359cd14d88ecb2e88c64b3fc5710
Sreenivas Reddy Akki <akkisreenivasulureddy@example.com>
2025-10-31 12:23:38 +0530

    Fix: update Flask run config for Render

--- app.py
+++ app.py
@@ -266,4 +266,5 @@ def automation_playwright():
 # Run Flask App
 # -----------------------------
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
